f217a360605e99998e701d405cbdb1ad590d5467
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-02 15:52:20 +0500

    update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let Counter = document.getElementById("counter");
 
 let Count = localStorage.getItem('count') ? parseInt(localStorage.getItem('count')) : 0;
 let Skolko = 1;
+
+counter.textContent = Count;
     
 function Click() {
     Count++;
-    counter.textContent = Count;
+    Counter.textContent = Count;
     localStorage.setItem('count', count);
 }
