2ca9fdc0968a3608d5cebdcec3f02d0b3b90c202
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-02 15:52:50 +0500

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let Counter = document.getElementById("counter");
 let Count = localStorage.getItem('count') ? parseInt(localStorage.getItem('count')) : 0;
 let Skolko = 1;
 
-counter.textContent = Count;
+Counter.textContent = Count;
     
 function Click() {
     Count++;
