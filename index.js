28beed9d7621330afccbab12fbaac1472eea69e6
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-02 15:42:46 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let Counter = document.getElementById("counter");
 
-let Count = 0
+let Count = 0;
 let Skolko = 1;
     
 function Click() {
-    Count++
-    counter.textContent = Count
+    Count++;
+    counter.textContent = Count;
 }
